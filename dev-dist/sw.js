0e807151a9bf4381400292b0ebb1f2242e727cac
dilshadbasith <dilshadbasith01@gmail.com>
2025-10-16 16:42:58 +0530

    updated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.l36f7t86d9g"
+    "revision": "0.0t43tdb6gf8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
